983a36fac45ff1b6349c0cd6ff8a203afdc1a3a8
Gareth <garethw738@gmail.com>
2023-09-06 15:28:37 +0200

    DONE SOME SHIT

--- clipboard_app.py
+++ clipboard_app.py
@@ -14,8 +14,15 @@ clipboard_contents = []
 def index():
     if request.method == 'POST' and 'save_server' in request.form:
         new_content = request.form['content']
-        clipboard_contents.append(new_content)
-        socketio.emit('new_content', new_content)  # Emitting a new_content event to all connected clients.
+        entry_name = request.form.get('entry_name', '').strip()
+        
+        # If the name field is empty, use a default name
+        if not entry_name:
+            entry_name = f"Copy {len(clipboard_contents) + 1}"
+
+        clipboard_contents.append((entry_name, new_content))
+        socketio.emit('new_content', {'name': entry_name, 'content': new_content})
+        
         return redirect(url_for('index'))
     return render_template('clipboard.html', contents=clipboard_contents)
 
@@ -24,6 +31,12 @@ def clear_clipboard():
     clipboard_contents.clear()  # Clear the list
     return "Clipboard cleared", 200
 
+@app.route("/delete/<int:item_index>", methods=['POST'])
+def delete_item(item_index):
+    if 0 <= item_index < len(clipboard_contents):
+        del clipboard_contents[item_index]
+        socketio.emit('delete_content', item_index)
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     socketio.run(app, debug=True, host='0.0.0.0', port=8080)
